5f32f5caa83ec76513110990a9fdefdfd5b28ea1
Adson Nael <adsonael.an@gmail.com>
2023-10-23 20:47:06 -0300

    added card and discipline routes

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,17 +1,37 @@
-import { Router } from "express"
-import UserController from "./controllers/UserController.js"
-import SessionsController from "./controllers/SessionsController.js"
-import auth from "./middlewares/auth.js"
+import { Router } from "express";
+import UserController from "./controllers/UserController.js";
+import SessionsController from "./controllers/SessionsController.js";
+import DisciplineController from './controllers/DisciplineController.js';
+import CardController from './controllers/CardController.js';
+import auth from "./middlewares/auth.js";
 
-const routes = new Router()
-routes.post("/sessions", SessionsController.create)
-routes.post('/user', UserController.create)
+const routes = new Router();
 
-routes.use(auth)
+// Rotas públicas (sem autenticação)
+routes.post("/sessions", SessionsController.create);
+routes.post('/user', UserController.create);
 
-// routes.get('/user', UserController.list) //rota apenas para testes
-routes.put('/user/:id', UserController.update)
-routes.get('/user/:id', UserController.show)
-routes.delete('/user/:id', UserController.delete)
+// Middleware de autenticação
+routes.use(auth);
 
-export default routes
+// Rotas protegidas (requerem autenticação)
+// Rotas para usuário
+routes.put('/user/:id', UserController.update);
+routes.get('/user/:id', UserController.show);
+routes.delete('/user/:id', UserController.delete);
+
+// Rotas para disciplina
+routes.get('/discipline', DisciplineController.list);
+routes.get('/discipline/:id', DisciplineController.show);
+routes.post('/discipline', DisciplineController.create);
+routes.put('/discipline/:id', DisciplineController.update);
+routes.delete('/discipline/:id', DisciplineController.delete);
+
+// Rotas para card
+routes.get('/card', CardController.list);
+routes.get('/card/:id', CardController.show);
+routes.post('/card', CardController.create);
+routes.put('/card/:id', CardController.update);
+routes.delete('/card/:id', CardController.delete);
+
+export default routes;
